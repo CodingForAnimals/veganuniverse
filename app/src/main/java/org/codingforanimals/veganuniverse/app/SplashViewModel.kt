18f1637484d26dcb52e057026859cd519d561ce0
Leonardo <agusmagne@gmail.com>
2023-02-06 23:17:09 -0300

    0005 - Tidy up

--- app/src/main/java/org/codingforanimals/veganuniverse/app/SplashViewModel.kt
+++ app/src/main/java/org/codingforanimals/veganuniverse/app/SplashViewModel.kt
@@ -2,7 +2,6 @@ package org.codingforanimals.veganuniverse.app
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlin.system.measureTimeMillis
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -23,22 +22,13 @@ class SplashViewModel(
 
     init {
         viewModelScope.launch {
-            val duration = measureTimeMillis {
-                val showOnboarding = async { onboardingPresenter.showOnboarding() }
-                val isUserLoggedIn = async { registrationPresenter.isUserLoggedIn() }
-                _uiState.value = LaunchState.Completed(
-                    showOnboarding = showOnboarding.await(),
-                    isUserLoggedIn = isUserLoggedIn.await(),
-                    startDestination = if (isUserLoggedIn.await()) CommunityDestination else WelcomeDestination
-                )
-            }
-            println("pepe argento duration $duration")
-        }
-    }
-
-    fun onboardingScreenDismissed() {
-        viewModelScope.launch {
-            onboardingPresenter.setOnboardingAsDismissed()
+            val showOnboarding = async { onboardingPresenter.showOnboarding() }
+            val isUserLoggedIn = async { registrationPresenter.isUserLoggedIn() }
+            _uiState.value = LaunchState.Completed(
+                showOnboarding = showOnboarding.await(),
+                isUserLoggedIn = isUserLoggedIn.await(),
+                startDestination = if (isUserLoggedIn.await()) CommunityDestination else WelcomeDestination
+            )
         }
     }
 
