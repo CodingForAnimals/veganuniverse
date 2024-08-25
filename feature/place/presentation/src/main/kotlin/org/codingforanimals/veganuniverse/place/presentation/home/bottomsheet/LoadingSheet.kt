87d9b5ca19bc633d69f435df3030b7913eae5024
agusmagne <agusmagne@gmail.com>
2024-08-24 23:59:47 -0300

    pre-release: 1.0.0-alpha2

--- feature/place/presentation/src/main/kotlin/org/codingforanimals/veganuniverse/place/presentation/home/bottomsheet/LoadingSheet.kt
+++ feature/place/presentation/src/main/kotlin/org/codingforanimals/veganuniverse/place/presentation/home/bottomsheet/LoadingSheet.kt
@@ -1,27 +1,22 @@
 package org.codingforanimals.veganuniverse.place.presentation.home.bottomsheet
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.placeholder.PlaceholderHighlight
-import com.google.accompanist.placeholder.placeholder
-import com.google.accompanist.placeholder.shimmer
 import org.codingforanimals.veganuniverse.commons.designsystem.Spacing_04
 import org.codingforanimals.veganuniverse.commons.designsystem.VeganUniverseTheme
 import org.codingforanimals.veganuniverse.commons.ui.animation.ShimmerItem
+import org.codingforanimals.veganuniverse.commons.ui.animation.shimmer
 import org.codingforanimals.veganuniverse.commons.ui.components.VUAssistChip
 import org.codingforanimals.veganuniverse.commons.ui.icon.VUIcons
 
@@ -48,13 +43,18 @@ internal fun LoadingSheet() {
                 iconDescription = "",
             )
         }
-        repeat(3) {
-            ShimmerItem(Modifier
-                .padding(Spacing_04)
-                .clip(ShapeDefaults.Medium)
-                .height(200.dp)
-                .fillMaxWidth()
-            )
+        Column(
+            modifier = Modifier.shimmer()
+        ) {
+            repeat(3) {
+                ShimmerItem(
+                    modifier = Modifier
+                        .padding(Spacing_04)
+                        .clip(ShapeDefaults.Medium)
+                        .height(200.dp)
+                        .fillMaxWidth()
+                )
+            }
         }
     }
 }
